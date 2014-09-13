7e20a7358465ab192963c692cf4caeae78bc1863
Pat Allan <pat@freelancing-gods.com>
2014-09-13 14:59:28 -0700

    Switching to Mandrill.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,15 +81,15 @@ CFPApp::Application.configure do
   config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] }
   config.action_mailer.default_options = {from: 'rubyconf@rubycentral.org'}
 
-  config.action_mailer.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
+  ActionMailer::Base.smtp_settings = {
+    :port =>           '587',
+    :address =>        'smtp.mandrillapp.com',
+    :user_name =>      ENV['MANDRILL_USERNAME'],
+    :password =>       ENV['MANDRILL_APIKEY'],
+    :domain =>         'heroku.com',
+    :authentication => :plain
   }
+  ActionMailer::Base.delivery_method = :smtp
 
   config.exceptions_app = self.routes
 end
